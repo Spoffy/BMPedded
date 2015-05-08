af65ca2ff84dba7dcefd8c4fbc47a26962a6baf2
Spoffy <spoffy@llagaming.net>
2015-05-08 22:54:16 +0100

    [Changed] Added caching to sample main

--- sample_main.c
+++ sample_main.c
@@ -7,30 +7,31 @@
 
 /* Lines have to be shorter than this. Note: buffer is on stack. */
 #define LINE_BUFF_LEN 55    
+//#define IMAGE_CACHE_SIZE 2048
 
 FIL File;  						/* FAT File */
 
-int position = 0;
-
 void read_image_bytes(bmp_data_request * request)
 {
-  UINT bytes_read = 0;
-  f_lseek(&File, request->dataOffset);
-  f_read(&File, request->buffer, request->bufferSize, &bytes_read);
+    UINT temp = 0;
+    f_lseek(&File, request->dataOffset);
+    f_read(&File, request->buffer, request->bufferSize, &temp);
 }
 
 void main(void) {
     os_init();
 
     bmp_image_loader_state imageState;
-    rectangle rect = {0, 120, 0, 60};
+    rectangle rect = {0, 320, 0, 240};
 
 		f_mount(&FatFs, "", 0);
 		f_open(&File,"tuxsimp", FA_READ);	
-    init_bmp(&imageState, read_image_bytes);
-    rect.right = imageState.dibHeader.imageWidth;
-    rect.bottom = imageState.dibHeader.imageHeight;
-    display_segment_bmp(30, 0, &rect, &imageState);
+    status_t stat = init_bmp(&imageState, read_image_bytes, 4096);
+    if(stat == STATUS_OK) {
+      rect.right = imageState.dibHeader.imageWidth;
+      rect.bottom = imageState.dibHeader.imageHeight;
+      display_segment_bmp(0, 0, &rect, &imageState);
+    }
 		f_close(&File);
 
 }
